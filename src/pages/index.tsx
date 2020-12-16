d0f57675503e9c357c1538d890c722417badc094
Achim Rolle <achimrolle@googlemail.com>
2020-12-16 20:53:29 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Nextplate</title>
+      </Head>
+
+      <main>Homepage Nextplate</main>
+    </>
+  );
+};
+
+export default Home;
